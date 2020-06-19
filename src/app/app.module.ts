9274861679f83fc19b8a7e471d0e473582c43363
paramSonawane <paramsonawane@gmail.com>
2020-06-19 17:50:51 +0530

    reformatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingComponent } from './header/shopping/shopping.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
-@Ng;
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component'Module({
+@NgModule({
   declarations: [
-    AppComponent,
+    AppComponent, 
     HeaderComponent,
-    ShoppingComponent,
     RecipesComponent,
     RecipeDetailComponent,
-  ,
-    RecipeListComponent],
+    RecipeListComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
+  ],
   imports: [
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
