513d6f7c3231981b9ab701694b5dc722844e30ce
paramSonawane <paramsonawane@gmail.com>
2020-06-19 15:56:54 +0530

    added basic structure of webapp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingComponent } from './header/shopping/shopping.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
-@NgModule({
+@Ng;
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component'Module({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HeaderComponent,
+    ShoppingComponent,
+    RecipesComponent,
+    RecipeDetailComponent,
+  ,
+    RecipeListComponent],
   imports: [
     BrowserModule
   ],
