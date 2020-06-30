4d69b2d2396bbdc7e7a72c8ce7485f37d995993b
paramSonawane <paramsonawane@gmail.com>
2020-06-30 17:29:35 +0530

    added some functioning to shopping tab

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Ingredient } from '../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient("Test ingredient", 45),
+    new Ingredient("Test2 ingredient", 5),
+    new Ingredient("Test3 ingredient", 74),
+  ];
 
   constructor() { }
 
