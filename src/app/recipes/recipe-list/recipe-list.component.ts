27d085c31fc621e40dbc0b8cd98f9c44d5f0db4c
paramSonawane <paramsonawane@gmail.com>
2020-07-08 18:40:17 +0530

    added new recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,20 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipeService } from '../recipe.service';
 import { Recipe } from '../recipe.model';
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+    selector: 'app-recipe-list',
+    templateUrl: './recipe-list.component.html',
+    styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipesList: Recipe[] = [
-    new Recipe("Test Recipe", "Test Recipe Description", "https://realfood.tesco.com/media/images/Ritas-enchiladas-recipe-1400x919-1c7ff22b-ea5e-44cf-9ada-d7b04420002f-0-1400x919.jpg"),
-    new Recipe("Test Recipe", "Test Recipe Description", "https://realfood.tesco.com/media/images/Ritas-enchiladas-recipe-1400x919-1c7ff22b-ea5e-44cf-9ada-d7b04420002f-0-1400x919.jpg"),
+    recipesList :Recipe[] = [];
 
-  ];
-  constructor() { }
+    constructor(private recipeService :RecipeService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.recipesList = this.recipeService.getRecipes();
+    }
 
 }
