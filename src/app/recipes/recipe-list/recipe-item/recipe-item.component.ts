194684efb9d0078efd3919a86850b8ee5f52d7dc
paramSonawane <paramsonawane@gmail.com>
2020-07-08 18:41:21 +0530

    added recipe service and attached its event emiter

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../../recipe.model';
+import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -8,9 +9,14 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipeItem:Recipe;
-  constructor() { }
+
+  constructor(private recipeService :RecipeService) { }
 
   ngOnInit(): void {
+  }
+
+  onSelected(){
+    this.recipeService.recipeSelected.emit(this.recipeItem);
   }
 
 }
