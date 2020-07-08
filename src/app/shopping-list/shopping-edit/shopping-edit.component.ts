79aca3ae88a218817b2c8bef87ac2a06d07e32ae
paramSonawane <paramsonawane@gmail.com>
2020-07-08 19:41:53 +0530

    shopping list servcie added to ingredients fomr

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef :ElementRef;
+  @ViewChild('amountInput') amountInputRef :ElementRef;
 
-  constructor() { }
+  constructor(private slService :ShoppingListService) { }
 
   ngOnInit(): void {
   }
 
+  onAddItem() {
+    const newIngredient = new Ingredient(
+      this.nameInputRef.nativeElement.value,
+      this.amountInputRef.nativeElement.value
+    );
+    this.slService.addIngredients(newIngredient);
+  }
 }
